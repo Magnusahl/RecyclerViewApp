a42c4fa991f1ab4ce4394bbcfd60380268e8029a
Magnus Ahlqvist <51696488+Magnusahl@users.noreply.github.com>
2020-04-22 11:52:12 +0200

    Added FAB button

---
+++ app/src/main/java/com/example/recyclerviewapp/AddAndCreateStudentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddAndCreateStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_and_create_student)
+    }
+}
--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.recyclerviewapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.studentsList)
-
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         recyclerView.adapter = StudentsRecyclerAdapter(this, DataManager.students)
 
-
+        val fab = findViewById<View>(R.id.floatingActionButton2)
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, AddAndCreateStudentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
